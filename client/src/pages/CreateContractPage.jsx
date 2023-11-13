cc1732e2f889fe960682c20851a3de49824683fa
subrat0018 <subratchandra2003@gmail.com>
2023-11-14 02:15:44 +0530

    feat: addOrganization, updatePateintData

--- client/src/pages/CreateContractPage.jsx
+++ client/src/pages/CreateContractPage.jsx
@@ -1,115 +1,102 @@
 // CreateContractPage.jsx
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useContext } from "react";
+import Web3Context from "../contexts";
+import { addOrganizationForPatient, updatePatientData } from "../contexts/useContract/writeContract";
+import { grantAccess } from "../contexts/useContract/writeContract";
 
 const CreateContractPage = () => {
+  const {account, _EnrollmentContract, _PatientOrgContract} = useContext(Web3Context);
   const [contractData, setContractData] = useState({
-    ownerName: '',
-    accessControl: [],
-    resourceId: '',
+    ownerName: "",
+    ownerAddress: "",
+    accessControl: "",
+    accessFile: ""
   });
 
-  const [newAccessControl, setNewAccessControl] = useState('');
+  const [newAccessControl, setNewAccessControl] = useState("");
 
   const handleInputChange = (e) => {
+    console.log(e.target);
     const { name, value } = e.target;
     setContractData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleAccessControlChange = (e) => {
-    const { value, checked } = e.target;
-    setContractData((prevData) => {
-      if (checked) {
-        return { ...prevData, accessControl: [...prevData.accessControl, value] };
-      } else {
-        return { ...prevData, accessControl: prevData.accessControl.filter((item) => item !== value) };
-      }
-    });
-  };
-
-  const handleAddAccessControl = () => {
-    if (newAccessControl.trim() !== '') {
-      setContractData((prevData) => ({
-        ...prevData,
-        accessControl: [...prevData.accessControl, newAccessControl],
-      }));
-      setNewAccessControl('');
-    }
-  };
-
   const handleCreateContract = () => {
     // Implement logic to create the Patient Organization Contract
-    console.log('Creating contract:', contractData);
+    addOrganizationForPatient(_PatientOrgContract, account.currentAccount)
+    updatePatientData()
     // Reset form after creating the contract
     setContractData({
-      ownerName: '',
-      accessControl: [],
-      resourceId: '',
+      ownerName: "",
+      ownerAddress: "",
+      accessControl: "",
+      accessFile: ""
     });
   };
 
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-4">Create Patient Organization Contract</h1>
+      <h1 className="text-3xl font-bold mb-4">
+        Create Patient Organization Contract
+      </h1>
 
       <form>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Owner Name:</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Organization Name:
+          </label>
           <input
             type="text"
             name="ownerName"
             className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter owner name"
+            placeholder="ABC Org"
             value={contractData.ownerName}
             onChange={handleInputChange}
           />
         </div>
-
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Access Control:</label>
-          <div className="mb-2">
-            {contractData.accessControl.map((access) => (
-              <div key={access} className="flex items-center mb-1">
-                <input
-                  type="checkbox"
-                  value={access}
-                  checked={contractData.accessControl.includes(access)}
-                  onChange={handleAccessControlChange}
-                  className="mr-2"
-                />
-                <span>{access}</span>
-              </div>
-            ))}
-          </div>
-          <div className="flex items-center">
-            <input
-              type="text"
-              value={newAccessControl}
-              onChange={(e) => setNewAccessControl(e.target.value)}
-              placeholder="New Access Control"
-              className="mr-2 shadow appearance-none border rounded py-1 px-2 leading-tight focus:outline-none focus:shadow-outline"
-            />
-            <button
-              type="button"
-              className="bg-green-500 text-white py-1 px-2 rounded"
-              onClick={handleAddAccessControl}
-            >
-              Add
-            </button>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Resource ID:</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Organization Address:
+          </label>
           <input
             type="text"
-            name="resourceId"
+            name="ownerAddress"
             className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter resource ID"
-            value={contractData.resourceId}
+            placeholder="0x0106B72164234f8Dca99D38415Ce00C133b93B70"
+            value={contractData.ownerAddress}
             onChange={handleInputChange}
           />
         </div>
 
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Access Control:
+          </label>
+          <div className="flex items-center">
+            <input
+              type="text"
+              name="accessControl"
+              value={contractData.accessControl}
+              onChange={handleInputChange}
+              placeholder="accessControl"
+              className="mr-2 shadow appearance-none border rounded py-1 px-2 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+        </div>
+        <div className=" mb-4">
+          <label
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            for="file_input"
+          >
+            Upload file
+          </label>
+          <input
+            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+          />
+        </div>
         <div className="flex justify-end">
           <button
             type="button"
