664bb3fddca5e72b09446a53ef8c8a55e7fe32a4
Sibasis-Malla <sibasisml@gmail.com>
2023-11-14 12:40:22 +0530

    minor fixes

--- client/src/utils/ipfs.js
+++ client/src/utils/ipfs.js
@@ -1,10 +1,10 @@
 //import {create} from 'ipfs-http-client'
-const Buffer = require('buffer')
+// const Buffer = require('buffer')
 const ipfsClient = require('ipfs-http-client')
 const projectId = '277J4qfFtNMoU4VjPgwkSC7agwj'
 const projectSecret = '25dbbdefd169318ba0dd5d82708f080a'
-const auth =
-  'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
+// const auth =
+//   'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
 //console.log(auth)
 const client = ipfsClient.create({
   host: 'ipfs.infura.io',
